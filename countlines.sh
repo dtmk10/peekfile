6f04779bf8cfdf98a7d5078b1c30d28e6f9a7b40
Diego Maquieira <dgmavida@gmail.com>
2025-11-06 00:06:19 +0100

    add: Now it can get any number of arguments

--- countlines.sh
+++ countlines.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-file=$1
-lines=$(cat $file | wc -l)
+file=$@
 
-ls $file
+echo $file
 
-if [[ $lines -eq 0 ]];
- then
-    echo there are $lines lines, $file is empty
-elif [[ $lines -eq 1 ]];
- then
-    echo there is $lines line
-    echo =====
-    head $file
-else
-    echo there is more than one line, there are $lines
-    echo =====
-    head $file
-fi
+for i in $file; 
+do
+    ls $i
+    lines=$(cat $i | wc -l)
+
+    if [[ $lines -eq 0 ]];
+    then
+        echo there are $lines lines, $i is empty
+    elif [[ $lines -eq 1 ]];
+    then
+        echo there is $lines line
+        echo =====
+        head $i
+    else
+        echo there is more than one line, there are $lines
+        echo =====
+        head $i
+    fi
+    echo ______________
+done
